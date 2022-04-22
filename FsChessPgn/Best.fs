647b2cbd88a6d670ee3b960f90bbefa21d07f9a3
Phil-Brooks <phil@brookshome.co.uk>
2022-04-22 20:40:21 +0100

    initialise storage for best

--- FsChessPgn/Best.fs
+++ FsChessPgn/Best.fs
@@ -9,13 +9,25 @@ module Best =
     let GetDict(fl:string) = 
         let ans = new System.Collections.Generic.Dictionary<string,Bmresps>()
         if File.Exists(fl) then
-            //TODO
+            let doln (ln:string) =
+                let bits = ln.Split([|'|'|])
+                let fen = bits.[0]
+                let bm = bits.[1]
+                let rs = bits.[2].Split([|','|])
+                let bmrs = {BestMove=bm;Replies=rs}
+                ans.Add(fen,bmrs)
+            let lns = File.ReadAllLines(fl)
+            lns|>Array.iter doln
             ans
         else
             ans
     ///save dictionary given location
     let SaveDict(fl:string, dct:System.Collections.Generic.Dictionary<string,Bmresps>) =
-        ()
+        let toln k (v:Bmresps) =
+            k + "|" + v.BestMove + "|" + 
+            (v.Replies|>Array.reduce(fun a b -> a + "," + b))
+        let lns = dct|>Seq.map(fun (KeyValue(k,v)) -> toln k v)|>Seq.toArray
+        File.WriteAllLines(fl,lns)
 
     ///Add to dictionary given fen, best move and list of responses
     let Add(dct:System.Collections.Generic.Dictionary<string,Bmresps>, fen:string, bm: string, sans:string[]) =
--- scripts/leela1.fsx
+++ scripts/leela1.fsx
@@ -6,15 +6,22 @@ let bestdictfl = @"D:\lc0\bestdict.txt"
 let dct = Best.GetDict(bestdictfl)
 
 Leela.SetFol(@"D:\lc0")
-let bd = Board.Start
-let bm = Leela.GetBestMove(bd,10)
 
-let nbd = bd|>Board.Push bm
+let initialise() =
+    let bd = Board.Start
+    let bm = Leela.GetBestMove(bd,10)
 
-let fen = bd|>Board.ToStr
-let bmstr = bm|>Move.ToSan bd
-let sans = OpenExp.GetMoves(nbd)
+    let nbd = bd|>Board.Push bm
 
-Best.Add(dct,fen,bmstr,sans)
-dct
-Best.SaveDict(bestdictfl,dct)
+    let fen = bd|>Board.ToStr
+    let bmstr = bm|>Move.ToSan bd
+    let sans = OpenExp.GetMoves(nbd)
+
+    Best.Add(dct,fen,bmstr,sans)
+    Best.SaveDict(bestdictfl,dct)
+
+if dct.Count=0 then initialise()
+else
+    //TODO
+    //iterate through keys filling blanks
+    ()
